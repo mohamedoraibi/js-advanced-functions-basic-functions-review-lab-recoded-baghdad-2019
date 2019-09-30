8591ccef94c7195f31dad1c5f1006a82eb8f96f4
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-30 11:22:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+
+
+
+
+function saturdayFun(activity="roller-skate") {
+  return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(activity="go to the office") {
+  return (`This Monday, I will ${activity}.`);
+}
+
+function wrapAdjective(parm="special",spec="*") {
+  return function(parm) {
+    return `You are ${spec}${parm}${spec}!`
+  }
+}
