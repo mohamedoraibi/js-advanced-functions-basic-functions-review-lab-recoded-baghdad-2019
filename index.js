cb4f281ce44f3095e46582014f0472bcd2cf7829
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-10-15 12:49:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Your code here
 
-Calculator={}
+let Calculator={
+  add: function add(x,y){return x+y},
+  subtract: function subtract(x,y){ return x-y},
+  multiply: function multiply(x,y){ return x*y},
+  divide: function divide(x,y){ return x/y},
+}
 
 
 function saturdayFun(activity="roller-skate") {
@@ -11,9 +16,18 @@ function mondayWork(activity="go to the office") {
   return (`This Monday, I will ${activity}.`);
 }
 
-function wrapAdjective(parm="special",spec="*") {
-  return function() {
-    return `You are ${spec}${parm}${spec}!`
+function wrapAdjective(spec="*" ) {
+  return function(parm="special") {
+    return "You are "+spec+parm+spec+"!"
 
+  }
+}
+
+function actionApplyer(x,y){
+  if(y.length === 0){
+    return 0;
+  }
+  else{
+  return ((x*2)+1000)%7;
   }
 }
